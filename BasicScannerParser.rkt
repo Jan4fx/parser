3fa3160a97fecb538eaecc047c4d87275145ae82
Jan4fx <jan4fx@umsystem.edu>
2023-02-21 11:04:43 -0600

    More Descriptive Error Messages

--- BasicScannerParser.rkt
+++ BasicScannerParser.rkt
@@ -56,10 +56,10 @@
      (tokenizer input-port))]
    
    [#\*
-    (error"Unknown Operator")]
+    (error"Error Unknown Operator While Scanning: *")]
    
    [#\/
-    (error"Unknown Operator")]
+    (error"Error Unknown Operator While Scanning: /")]
 
    [#\=
     (cons `=
@@ -90,40 +90,40 @@
       ;every line has to start with linenumber or EOF symbol
       [(equal? (first tokens)`INT) (stmt_list (rest tokens))]
       [(equal? (first tokens)`$$) (print"accept")]
-      [else (error"Syntax Error: Expected Line Number or EOF")]))
+      [else (error"Syntax Error: Expected Line Number or EOF; Instead got:" (first tokens))]))
 
 
 (define (goidx tokens)
   (cond
     [(equal? (first tokens)`INT) (gotail (rest tokens))]
-    [else (error"Syntax Error: Expected INT for goto/gosub")]))
+    [else (error"Syntax Error: Expected INT for goto/gosub; Instead got:" (first tokens))]))
     
 
 (define (gotail tokens)
     (cond
     [(equal? (first tokens)`newline) (program (rest tokens))]
-    [else (error"Syntax Error: Expected newline")]))
+    [else (error"Syntax Error: Expected newline; Instead got:" (first tokens))]))
 
 (define (readidx tokens)
     (cond
     [(equal? (first tokens)'ID) (readtail (rest tokens))]
     [(equal? (first tokens)'INT) (readtail (rest tokens))]
     [(equal? (first tokens)'LPAR) (parstmt (rest tokens))]
-    [else (error"Syntax Error: Expected ID INT or LPAR for read/write")]))
+    [else (error"Syntax Error: Expected ID INT or LPAR for read/write; Instead got:" (first tokens))]))
 
 (define (parstmt tokens)
     (cond
     [(equal? (first tokens)'ID) (parexpr (rest tokens))]
     [(equal? (first tokens)'INT) (parexpr (rest tokens))]
     [(equal? (first tokens)'RPAR) (readtail (rest tokens))]
-    [else (error"Syntax Error: Expected ID/INT/RPAR")]))
+    [else (error"Syntax Error: Expected ID/INT/RPAR; Instead got:" (first tokens))]))
 
 (define (parexpr tokens)
     (cond
     [(equal? (first tokens)'+) (parstmt (rest tokens))]
     [(equal? (first tokens)'-) (parstmt (rest tokens))]
     [(equal? (first tokens)'RPAR) (readtail (rest tokens))]
-    [else (error"Syntax Error: Expected + - or RPAR")]))
+    [else (error"Syntax Error: Expected + - or RPAR; Instead got:" (first tokens))]))
 
 
 
@@ -134,24 +134,24 @@
     [(equal? (first tokens)`-) (readidx (rest tokens))]
     [(equal? (first tokens)`=) (readidx (rest tokens))]
     [(equal? (first tokens)`:) (returnstmt (rest tokens))]
-    [else (error"Syntax Error: Expected newline or math statement")]))
+    [else (error"Syntax Error: Expected newline or math statement; Instead got:" (first tokens))]))
 
 (define (returnstmt tokens)
     (cond
     [(equal? (first tokens)`return) (returntail (rest tokens))]
-    [else (error"Syntax Error: Expected return")]))
+    [else (error"Syntax Error: Expected return; Instead got:" (first tokens))]))
 
 (define (returntail tokens)
     (cond
     [(equal? (first tokens)`newline) (program (rest tokens))]
-    [else (error"Syntax Error: Expected newline")]))
+    [else (error"Syntax Error: Expected newline; Instead got:" (first tokens))]))
 
 (define (ifstmt tokens)
     (cond
     [(equal? (first tokens)'ID) (iftail (rest tokens))]
     [(equal? (first tokens)'INT) (iftail (rest tokens))]
     [(equal? (first tokens)'LPAR) (ifparstmt (rest tokens))]
-    [else (error"Syntax Error: Expected newline ID or INT")]))
+    [else (error"Syntax Error: Expected ID or INT; Instead got:" (first tokens))]))
 
 (define (iftail tokens)
     (cond
@@ -159,28 +159,28 @@
     [(equal? (first tokens)'-) (ifstmttail (rest tokens))]
     ;;;;;;
     [(equal? (first tokens)'=) (ifstmttail (rest tokens))]
-    [else (error"Syntax Error: Expected newline or math statement")]))
+    [else (error"Syntax Error: Expected op; Instead got:" (first tokens))]))
 
 (define (ifstmttail tokens)
     (cond
     [(equal? (first tokens)'ID) (thenstmt (rest tokens))]
     [(equal? (first tokens)'INT) (thenstmt (rest tokens))]
     [(equal? (first tokens)'LPAR) (ifparstmt (rest tokens))]
-    [else (error"Syntax Error: Expected math statement")]))
+    [else (error"Syntax Error: Expected math statement; Instead got:" (first tokens))]))
 
 (define (ifparstmt tokens)
     (cond
     [(equal? (first tokens)'ID) (ifparexpr (rest tokens))]
     [(equal? (first tokens)'INT) (ifparexpr (rest tokens))]
     [(equal? (first tokens)'RPAR) (thenstmt (rest tokens))]
-    [else (error"Syntax Error: Expected ID/INT/RPAR")]))
+    [else (error"Syntax Error: Expected ID/INT/RPAR; Instead got:" (first tokens))]))
 
 (define (ifparexpr tokens)
     (cond
     [(equal? (first tokens)'+) (ifparstmt (rest tokens))]
     [(equal? (first tokens)'-) (ifparstmt (rest tokens))]
     [(equal? (first tokens)'RPAR) (thenstmt (rest tokens))]
-    [else (error"Syntax Error: Expected + - or RPAR")]))
+    [else (error"Syntax Error: Expected op or RPAR; Instead got:" (first tokens))]))
 
 
 ;check for par
@@ -188,7 +188,7 @@
     (cond
     [(equal? (first tokens)'newline) (thenstmt (rest tokens))]
     [(equal? (first tokens)'then) (thentail (rest tokens))]
-    [else (error"Syntax Error: Expected then statement")]))
+    [else (error"Syntax Error: Expected then statement; Instead got:" (first tokens))]))
 
 (define (thentail tokens)
     (cond
@@ -196,7 +196,7 @@
     [(equal? (first tokens)'write) (readidx (rest tokens))]
     [(equal? (first tokens)'goto) (goidx (rest tokens))]
     [(equal? (first tokens)'gosub) (goidx (rest tokens))]
-    [else (error"Syntax Error: Expected read/write/goto/gosub")]))
+    [else (error"Syntax Error: Expected read/write/goto/gosub; Instead got:" (first tokens))]))
 
 
 
@@ -209,7 +209,7 @@
       [(equal? (first tokens)`gosub) (goidx (rest tokens))]
       [(equal? (first tokens)`ID) (readtail (rest tokens))]
       [(equal? (first tokens)`if) (ifstmt (rest tokens))]
-      [else (error "Syntax Error")]))
+      [else (error "Syntax Error: Expected stmt; Instead got:" (first tokens))]))
 
 (define tokens(tokenizer(open-input-file "file03.txt"))) ;test token stream
 ;tokens
@@ -221,6 +221,7 @@
 ;test parse function
 
 ;test parse function
+
 ;(parse "file01.txt")
 ;(parse "file02.txt")
 ;(parse "file03.txt")
